6fa61e772ae45efb92ff7a049143dadbbcf18d2c
Pedro Augusto Costa Anjos <pedrooaugustoo130@gmail.com>
2022-09-10 09:43:24 -0300

    Project created

---
+++ src/main/java/com/pedroacbg/api/zntcentral/ZntcentralApplication.java
@@ -0,0 +1,13 @@
+package com.pedroacbg.api.zntcentral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZntcentralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZntcentralApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedroacbg/api/zntcentral/ZntcentralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedroacbg.api.zntcentral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZntcentralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
